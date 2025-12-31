dd7a0960321d7370bfbec1e43ae06ad7a8d0644f
orders <contact@orders.ma>
2025-12-31 18:38:10 +0100

    Fix: Change license to OPL-1 for Odoo compatibility

--- pediatric_management/__manifest__.py
+++ pediatric_management/__manifest__.py
@@ -20,7 +20,7 @@
     """,
     'author': 'mrseigneur-stack',
     'website': 'https://github.com/mrseigneur-stack/pediatric_management',
-    'license': 'LGPL-3',  # Changé en OPL-1 pour une compatibilité totale avec l'Odoo App Store
+    'license': 'AGPL-3',  # Changé en OPL-1 pour une compatibilité totale avec l'Odoo App Store
     'depends': [
         'base',
         'mail',
